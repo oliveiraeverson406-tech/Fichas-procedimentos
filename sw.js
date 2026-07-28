dfd5015b4c3cf4ea3c81869df6ceda3647798c05
oliveiraeverson406-tech <oliveiraeverson406@gmail.com>
2026-07-28 07:54:38 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NOME = "ficha-procedimentos-v1";
+const ARQUIVOS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NOME).then((cache) => cache.addAll(ARQUIVOS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((resposta) => {
+        const copia = resposta.clone();
+        caches.open(CACHE_NOME).then((cache) => cache.put(event.request, copia)).catch(() => {});
+        return resposta;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
